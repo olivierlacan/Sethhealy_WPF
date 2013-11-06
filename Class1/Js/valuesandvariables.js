2914ee3f6674d6286d6e74421ba4b6b8fec83ecf
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-06 15:42:39 -0500

    activity values and variables
    i completed the activity values and variables

---
+++ Class1/Js/valuesandvariables.js
@@ -0,0 +1,72 @@
+/**
+ * Created by sethhealy on 11/6/13.
+ */
+var yearborn = 1988;
+
+//console.log("Hello World!");
+//console.log(yearborn);
+//
+//alert(yearborn);
+//
+//var age;
+//age = 25;
+//
+//var yearofbirth = 1988;
+
+//var age = 25;
+//
+//var isstudent= false;
+//var firstname = "Seth";
+//var phras ='i don\'t know\n you\'ll have to speak with the professor';
+ var muppetnames = ["kermit","miss piggy"];
+
+muppetnames[1] = "rizzo";
+muppetnames[2] = "fozzy";
+
+var num = 1;
+console.log(muppetnames[num]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
