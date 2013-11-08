603d1e47b5d2374b15fc907a5231ebc00f79a3da
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-08 09:04:34 -0500

    commit to expressions worksheet
    more commitments to expressions worksheet

---
+++ Class1/Js/expressionsworksheet.js
@@ -0,0 +1,20 @@
+/**
+ * Created by sethhealy on 11/6/13.
+ */
+   //problem 1
+//var sparkysage =prompt ("How old is sparky");
+//
+//age=sparkysage*7;
+//
+//console.log(age);
+
+   //problem 2 and 3
+//
+//var slices= prompt("how many slices are there per pizza?");
+//var people= prompt("how many people are at the party?");
+//var ordered= prompt("how many pizzas were ordered?");
+//
+//pizza=(slices*ordered)%people;////modules instead of the divide sign
+//
+//console.log(pizza);
+
--- Class1/Js/valuesandvariables.js
+++ Class1/Js/valuesandvariables.js
@@ -18,13 +18,13 @@ var yearborn = 1988;
 //var isstudent= false;
 //var firstname = "Seth";
 //var phras ='i don\'t know\n you\'ll have to speak with the professor';
- var muppetnames = ["kermit","miss piggy"];
-
-muppetnames[1] = "rizzo";
-muppetnames[2] = "fozzy";
-
-var num = 1;
-console.log(muppetnames[num]);
+// var muppetnames = ["kermit","miss piggy"];
+//
+//muppetnames[1] = "rizzo";
+//muppetnames[2] = "fozzy";
+//
+//var num = 1;
+//console.log(muppetnames[num]);
 
 
 
