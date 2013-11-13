3a17c49f97c860d29ed84ed2f43d2ccb09a3ba38
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-13 14:19:34 -0500

    commit
    commiting assignment again

--- Class1/Js/expression.js
+++ Class1/Js/expression.js
@@ -3,12 +3,11 @@
  */
     // personal. Copper has to go to the bathroom every 6 hours so depending on what time you awake is how many times
 //goes out so im calculating how many times within a 24 hour period he goes out to go to the bathroom.
-
-var wake= parseInt(prompt('What time did you wake up'));
-
-for(i=wake;i<=24;i=i+6){
-
-    console.log(i);
-}
-
+//
+//var wake= parseInt(prompt('What time did you wake up'));
+//
+//for(i=wake;i<=24;i=i+6){
+//
+//    console.log(i);
+//}
 
