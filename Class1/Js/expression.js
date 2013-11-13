4372cfee8185314b96ee6503eb2ff85be337e88a
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-13 15:41:03 -0500

    commit to industry
    finished but not yet complete

--- Class1/Js/expression.js
+++ Class1/Js/expression.js
@@ -1,13 +1,35 @@
 /**
  * Created by sethhealy on 10/30/13.
  */
-    // personal. Copper has to go to the bathroom every 6 hours so depending on what time you awake is how many times
+    // personal.
+
+    // Copper has to go to the bathroom every 6 hours so depending on what time you awake is how many times
 //goes out so im calculating how many times within a 24 hour period he goes out to go to the bathroom.
 //
 //var wake= parseInt(prompt('What time did you wake up'));
-//
-//for(i=wake;i<=24;i=i+6){
+//var b=6
+//for(i=wake;i<=24;i=i+b){
 //
 //    console.log(i);
 //}
 
+/// industry. im going to make a discount calculator.
+
+//var price= parseInt(prompt('How much is the product?'));
+//var discount = parseInt(prompt('How much of a discount do you have 10%,15% or 20%?'));
+//var percentages=0;
+//
+//if(discount=='10'){
+//    percentages=.1;
+//}else if(discount=='15'){
+//    percentages=.15;
+//}else if(discount=='20'){
+//    percentages=.2;
+//}
+//
+//var remainder=price-(price*percentages);
+//
+//console.log('You pay');
+//console.log(remainder);
+
+///wacky
