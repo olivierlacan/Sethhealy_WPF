4a1b3ac711664d19bf771ddffe0e172f69a9279e
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-15 13:12:00 -0500

    commit for conditional assignment
    industry sub section completed but not finished

---
+++ Class1/Js/conditional.js
@@ -0,0 +1,19 @@
+/**
+ * Created by sethhealy on 11/15/13.
+ */
+//var price= parseInt(prompt('How much is the product?'));
+//var discount = parseInt(prompt('How much of a discount do you have 10%,15% or 20%?'));
+//var percentages=0;
+//
+//if(discount=='10'){
+//    percentages=.1;
+//}else if(discount=='15'){
+//    percentages=.15;
+//}else if(discount=='20'){
+//    percentages=.2;
+//}
+//
+//var remainder=price-(price*percentages);
+//
+//console.log('You pay');
+//console.log(remainder);
