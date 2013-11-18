416905cbd64e7973dbcb57452fd7debc11e42e70
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-18 15:23:47 -0500

    commiting the function for industry
    adding my function in my assignment

--- Class1/Js/function_industry.js
+++ Class1/Js/function_industry.js
@@ -5,4 +5,9 @@
 var a=parseInt(prompt('What is the value for a?'));
 var b=parseInt(prompt('What is the value for b?'));
 
+function pythagorean(a,b){
 
+    var c = (a*a)+(b*b);
+    var final = Math.sqrt(c);
+    return final;
+}
