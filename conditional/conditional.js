9583b9312a7ad34830b83c79083cbf94e6657c7a
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-18 13:19:18 -0500

    commit to the industry conditional
    adding comments to the assignment

--- conditional/conditional.js
+++ conditional/conditional.js
@@ -4,11 +4,11 @@
 
     /// Im making a discount calculator letting the user type the price and how much they want a discount from the 3 choices
 /// and if they pick one that isnt there it will prompt invalid selection
-var price= parseInt(prompt('How much is the product?'));
+var price= parseInt(prompt('How much is the product?'));     // Gave the prompts so the user will choose the price and which discount
 var discount = parseInt(prompt('How much of a discount do you have 10%,15% or 20%?'));
 var percentages=0;
 
-if (discount!='10' || discount!='15' || discount!='20'){console.log('invalid selection')}
+if (discount!='10' || discount!='15' || discount!='20'){console.log('invalid selection')} //I gave each of the discounts a value excluding the invalid
 if(discount=='10'){
     percentages=.1;
 }else if(discount=='15'){
