9f848317693d69ce10a4d3c1e581a1fa45414ea6
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-18 14:05:10 -0500

    commit the wacky conditional
    did a time calculator to see if you have enough time in the day to
    watch tv

--- conditional/wacky.js
+++ conditional/wacky.js
@@ -1,4 +1,18 @@
 /**
  * Created by sethhealy on 11/18/13.
  */
+    //// I made a if statement to show if you have time to be watching netflix for as long as you do.
+var tv= prompt('How many hours have you been watching Netflix');
+var work= prompt('How long is homework assignment going to take?');
+var sleep= prompt('How long do you need to sleep for?');
+var left= 24-sleep-work-tv; //I wanted to show how much time in the day you have left after watching tv and doing homework.
+console.log('You have '+left+' hours left');
+
+if(left<2){
+    console.log('Not much time left'); //i showed that you dont have that much time to be watching tv.
+}
+else
+{
+    console.log('get to relax some'); // i put this to show you that you have some more time to do what you want.
+}
 
