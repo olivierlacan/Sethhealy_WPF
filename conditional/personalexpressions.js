c9c4c19493fe6daa88ebdd7dd256b4665f6fe509
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-18 14:04:25 -0500

    commit comments
    commiting my comments

--- conditional/personalexpressions.js
+++ conditional/personalexpressions.js
@@ -6,16 +6,16 @@
 
 var balance=parseInt(prompt('How much is in your account now?'));
 var bills=parseInt(prompt('How much did you pay in bills this month?'));
-var left=balance-bills;
+var left=balance-bills;     //I wanted to see what i have left after bills so i calculated balance- the bills
 console.log('You have ' + left + ' after bills');
 
 if(left >100){
-    console.log('You have enough for a date!!!!')
+    console.log('You have enough for a date!!!!');   //if i have more then 100 dollars a month that means i can go on a date.
 }
 if( left <= 0){
-    console.log('Maybe you should consider getting a job');
+    console.log('Maybe you should consider getting a job'); // If i had 0 or less i wanted it to give a warning saying your way too low
 
 }else if (left<=100){
 
-console.log('Not enough this month maybe next month :(');
+console.log('Not enough this month maybe next month :('); // if i had less then 100 and more then 0 i wanted it to say maybe next time.
 }
