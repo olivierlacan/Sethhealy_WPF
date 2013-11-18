8361d9b9d6aa6d2da4300057faf974b32a753c2d
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-18 15:27:47 -0500

    commiting comments industry
    adding commit to the assignment

--- Class1/Js/function_industry.js
+++ Class1/Js/function_industry.js
@@ -2,16 +2,16 @@
  * Created by sethhealy on 11/18/13.
  */
 
-var a=parseInt(prompt('What is the value for a?'));
+var a=parseInt(prompt('What is the value for a?')); ///adding prompts for the pythagorean.
 var b=parseInt(prompt('What is the value for b?'));
 
-function pythagorean(a,b){
+function pythagorean(a,b){   //making the function so i can recall it
 
     var c = (a*a)+(b*b);
     var final = Math.sqrt(c);
     return final;
 }
 
-var final = pythagorean(a,b);
+var final = pythagorean(a,b);  // pulling the function so i can console.log it.
 
 console.log(final);
