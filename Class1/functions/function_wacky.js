a73978fee33b7cab952baa74fe0be94a6ee6f1e2
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-20 14:10:55 -0500

    commit the wacky function
    i made a random color function for 5 differant colors

--- Class1/functions/function_wacky.js
+++ Class1/functions/function_wacky.js
@@ -2,6 +2,16 @@
  * Created by sethhealy on 11/20/13.
  */
 
+function random(type){
+
+    colors= Math.random();
 
 
+    type=colors <=.2 ? 'Blue' : type = colors <=.4 ? 'Green' : type = colors <=.6 ? 'Orange' : type = colors <=.8 ? 'Purple' : 'Yellow';
 
+    return type;
+
+}
+var type= random(type);
+
+console.log('Random color is '+type);
