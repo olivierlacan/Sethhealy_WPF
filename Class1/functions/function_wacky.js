85407c82872324d8b92b44d30115c29ca1922912
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-11-20 14:13:10 -0500

    commit wacky function
    i added comments to the function

--- Class1/functions/function_wacky.js
+++ Class1/functions/function_wacky.js
@@ -4,14 +4,14 @@
 
 function random(type){
 
-    colors= Math.random();
+    colors= Math.random(); // i made colors to randomize
 
 
     type=colors <=.2 ? 'Blue' : type = colors <=.4 ? 'Green' : type = colors <=.6 ? 'Orange' : type = colors <=.8 ? 'Purple' : 'Yellow';
-
+// I put values to the colors
     return type;
 
 }
-var type= random(type);
+var type= random(type);  // i pulled the var out.
 
-console.log('Random color is '+type);
+console.log('Random color is '+type); // commented to have it tell you that its randomized.
