8752ab4de34b5a4feff58f8106fc82768ea980a1
Sethhealy <sethhealy@Seths-MacBook-Pro.local>
2013-12-03 17:15:04 -0500

    commit duel part one
    commiting duel part one and class work

---
+++ Class1/Js/Class1wpa.js
@@ -0,0 +1,27 @@
+/**
+ * Created by sethhealy on 11/26/13.
+ */
+//function getArea(l,w){
+//  return l*w;
+//}
+//var a=getArea(3,2);
+//console.log(a);
+
+//var coin=Math.random();
+//
+//if (coin<.5);
+//{
+// console.log('heads')
+//}
+//
+//if(coin>.5){
+//
+//   console.log('tails');
+//}
+
+///grades.splice(1,1)   removes 1 number and the number of times it removes
+
+
+////have a deck of cards with a function called deal and a function called shuffle. For every time i call the shuffle function
+//it will call the deck
+
